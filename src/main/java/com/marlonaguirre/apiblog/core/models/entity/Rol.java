3c98e8c661f848e08272123aa264502b7115d074
Marlon Aguirre <marlon.aguirre.dev@gmail.com>
2019-12-27 11:10:07 -0600

    entity rol

--- src/main/java/com/marlonaguirre/apiblog/core/models/entity/Rol.java
+++ src/main/java/com/marlonaguirre/apiblog/core/models/entity/Rol.java
@@ -1,4 +1,28 @@
 package com.marlonaguirre.apiblog.core.models.entity;
 
-public class Rol {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.io.Serializable;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "rol")
+public class Rol  implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotEmpty(message = "Debe proporcionar un nombre para el rol")
+    @Column(name = "nombre")
+    private String nombre;
+
+
 }
