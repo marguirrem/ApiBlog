21c32131dab71992f7ff2bd2ee34a8949e4086f9
Marlon Aguirre <marlon.aguirre.dev@gmail.com>
2019-12-27 16:10:42 -0600

    user service

---
+++ src/main/java/com/marlonaguirre/apiblog/core/models/services/IUserService.java
@@ -0,0 +1,17 @@
+package com.marlonaguirre.apiblog.core.models.services;
+
+
+import com.marlonaguirre.apiblog.core.models.entity.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface IUserService {
+    public List<Usuario> findAll();
+    public Page<Usuario>findAll(Pageable pageable);
+    public Usuario save(Usuario usuario);
+    public Usuario findById(Long id);
+    public void delete(Usuario usuario);
+    public void delete(Long id);
+}
